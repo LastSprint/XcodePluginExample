4ec0c17b32a33fa80ea31c19f83a3d6368528d6b
Александр Кравченков <ia.noob@yandex.ru>
2018-10-21 21:03:40 +0300

    added tests

--- ExamplePluginTests/Method tests/ParseMethodParametersTest.swift
+++ ExamplePluginTests/Method tests/ParseMethodParametersTest.swift
@@ -12,9 +12,81 @@ import XCTest
 @testable
 import ExamplePlugin
 
-
 public class ParseMethodParametersTest: XCTestCase {
-    func testFullDeclaration() -> Void {
-        
+
+    func testFullDeclaration() {
+
+        // Arrange
+
+        let name = "foo"
+        let type = "Bar"
+
+        let source = "\(name): \(type), \(name): \(type)"
+
+        // Act
+
+        var result: [Parameter]!
+
+        do {
+            result = try MethodParser.parseParameters(string: source)
+        } catch {
+            XCTFail("\(error)")
+            return
+        }
+
+        // Assert
+
+        XCTAssertNotNil(result)
+        XCTAssertEqual(result.count, 2)
+        XCTAssertNotNil(result.first!.name, name)
+        XCTAssertNotNil(result.first!.type, type)
+        XCTAssertNotNil(result.last!.name, name)
+        XCTAssertNotNil(result.last!.type, type)
+    }
+
+    func testEmptyDeclaration() {
+
+        // Arrange
+
+        let source = ""
+
+        // Act
+
+        var result: [Parameter]!
+
+        do {
+            result = try MethodParser.parseParameters(string: source)
+        } catch {
+            XCTFail("\(error)")
+            return
+        }
+
+        // Assert
+
+        XCTAssertNotNil(result)
+        XCTAssertTrue(result.isEmpty)
+    }
+
+    func testBadDeclarationFeiled() {
+
+        // Arrange
+
+        let source = "foo bar"
+
+        // Act
+
+        var resultError: ParameterParser.ParseError!
+
+        do {
+            _ = try MethodParser.parseParameters(string: source)
+        } catch {
+            resultError = error as? ParameterParser.ParseError
+            return
+        }
+
+        // Assert
+
+        XCTAssertNotNil(resultError)
+        XCTAssertEqual(resultError, ParameterParser.ParseError.cantParseParameter)
     }
 }
